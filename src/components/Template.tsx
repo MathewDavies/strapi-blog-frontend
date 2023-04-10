017440ee829ae6a2fbd9f1b5a8d6d36471a1cc3f
Mathew Davies <mtdavies_mtd@hotmail.com>
2023-04-10 23:12:26 +0100

    Scroll to the top component added

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,10 +1,12 @@
 import {Outlet} from 'react-router-dom'
 import NavBar from './NavBar'
+import ScrollToTop from './ScrollToTop'
 
 
 function Template() {
   return (
     <div className='page'>
+    <ScrollToTop/>
     <NavBar/>
     <Outlet/>
     </div>
